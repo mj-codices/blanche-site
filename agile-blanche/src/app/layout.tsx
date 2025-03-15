073c7f3a7313adccad0a2feb09642632a659df3c
mj-codices <mjwhite.dev@gmail.com>
2025-03-14 17:28:28 -0700

    Added bootstrap

--- agile-blanche/src/app/layout.tsx
+++ agile-blanche/src/app/layout.tsx
@@ -1,15 +1,25 @@
+"use client"
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
+import { useEffect } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Bootnav from "./components/Bootnav"
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <html lang="en">
-      <body
-      
-      >
+      <body>
+        <Navbar/>
+        <Bootnav/>
         {children}
       </body>
     </html>
