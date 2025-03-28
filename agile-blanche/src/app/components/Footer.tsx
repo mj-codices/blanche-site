c02e5935a7992e49c2448011ec37ff6ab2c32317
mj-codices <mjwhite.dev@gmail.com>
2025-03-27 20:27:06 -0700

    Created footer and added basic styling

---
+++ agile-blanche/src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <footer className="fixed bottom-0 left-0 w-full bg-[#171717] text-white py-4 text-center">
+        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  
