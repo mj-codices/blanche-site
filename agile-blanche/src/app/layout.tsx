8861c4858cc576e09568d42fa2d698edb4b239ad
mj-codices <mjwhite.dev@gmail.com>
2025-04-01 14:04:52 -0700

    Imported subfont and created font class

--- agile-blanche/src/app/layout.tsx
+++ agile-blanche/src/app/layout.tsx
@@ -11,8 +11,13 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@98.1,300..700&display=swap" rel="stylesheet" />
+      </head>
       <body>
-       <Navbar/>
+        <Navbar/>
         {children}
       </body>
     </html>
