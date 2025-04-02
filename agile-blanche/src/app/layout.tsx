bd7e29f892d30ed674f83ae073891789e4b84842
mj-codices <mjwhite.dev@gmail.com>
2025-04-02 09:42:03 -0700

    Fixed layout crossOrigin error for font

--- agile-blanche/src/app/layout.tsx
+++ agile-blanche/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         <link href="https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@98.1,300..700&display=swap" rel="stylesheet" />
       </head>
       <body>
