beb575a11826f1b05094a76d4e83ad26c6f2750a
mj-codices <mjwhite.dev@gmail.com>
2025-04-02 15:45:15 -0700

    Centered main heading using padding

--- agile-blanche/src/app/page.tsx
+++ agile-blanche/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
     <section className="flex items-center justify-center">
-      <div className="pt-20 lg:pt-30 pl-8">
+      <div className="pt-20 lg:pt-30 pl-14">
         <span className="fredoka text-7xl lg:text-8xl tracking-wide">AGILE-</span>
         <span className="fredoka text-[#88807B] text-7xl lg:text-8xl tracking-wide">BLANCHE</span>
       </div>
