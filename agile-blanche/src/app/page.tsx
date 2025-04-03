9344ebacad055bbb0482a686040f7828384ece61
mj-codices <mjwhite.dev@gmail.com>
2025-04-03 15:38:26 -0700

    Removed unsed code

--- agile-blanche/src/app/page.tsx
+++ agile-blanche/src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
         <span className="fredoka text-7xl lg:text-8xl tracking-wide">AGILE-</span>
         <span className="fredoka text-[#88807B] text-7xl lg:text-8xl tracking-wide">BLANCHE</span>
       </div>
-      {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> */}
     </section>
     <section className="flex items-center justify-center">
       <div className="pt-15 pl-2">
