732ad1288d3a2a4f2a8bd1898476e1cb971bf655
mj-codices <mjwhite.dev@gmail.com>
2025-04-04 22:43:35 -0700

    Added button to expanded view

--- agile-blanche/src/app/page.tsx
+++ agile-blanche/src/app/page.tsx
@@ -1,16 +1,24 @@
+import { Button } from '@headlessui/react'
+
 export default function Home() {
   return (
     <div>
     <section className="flex items-center justify-center">
-      <div className="pt-20 lg:pt-30 pl-14">
+      <div className="pt-8 lg:pt-20 pl-10">
         <span className="fredoka text-7xl lg:text-8xl tracking-wide">AGILE-</span>
         <span className="fredoka text-[#88807B] text-7xl lg:text-8xl tracking-wide">BLANCHE</span>
       </div>
     </section>
     <section className="flex items-center justify-center">
-      <div className="pt-15 pl-2">
+      <div className="pt-10 pl-2">
         <span className="font-[myFirstFontBold] text-[#e9905a] text-5xl tracking-wider">Software Development </span>
         <span className="text-5xl tracking-widest">Services</span>
+        <div className="flex items-center justify-center pt-10 pl-1">
+          <p className="font-[myFirstFontBold]">Redefining software development with modern, nimble and results-driven solutions</p>
+        </div>
+        <div className="flex items-center justify-center">
+            <Button className="rounded-full bg-[#171717] font-[myFirstFont] text-lg text-white pl-3.5 pr-3.5 pb-3 pt-3.5 mt-10">Contact Us</Button>
+        </div>
       </div>
     </section>
     </div>
