e7e60db18392ccca7fbc5ca79bc3a9d9f5960096
mj-codices <mjwhite.dev@gmail.com>
2025-04-10 14:01:32 -0700

    Removed unused code

--- agile-blanche/src/app/components/Navbar.tsx
+++ agile-blanche/src/app/components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
       href={item.href}
       aria-current={item.current ? 'page' : undefined}
       className={classNames(
-        item.name === 'Contact' ? 'font-[myFirstFont] rounded-full bg-[#171717] text-white text-lg hover:font-[myFirstFont]' : 'font-[myFirstFont]',
+        item.name === 'Contact' ? 'font-[myFirstFont] rounded-full bg-[#171717] text-white text-lg' : 'font-[myFirstFont]',
         item.current ? 'text-[#e9905a] bold-action-text drop-shadow-lg' : 'text-[#171717] transition delay-50 duration-200 ease-in-out hover:text-[#e9905a] hover:drop-shadow-lg',
         'px-3 py-2.5 text-lg lg:text-xl custom-md-lg'
       )}
@@ -72,7 +72,7 @@ export default function Navbar() {
                 {navigation.filter((item) => item.name === "Contact").map((item) => (
                   renderNavItem(item)
                 ))}
-                </div>
+              </div>
             </div>
           </div>
           {/* The DisclosurePanel holds the dropdown logic we need for our Navbar and renders the "mobile view" nav links */}
