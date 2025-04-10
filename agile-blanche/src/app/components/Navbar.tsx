cce13cdb21f6649b82a327c570b53e7e99756584
mj-codices <mjwhite.dev@gmail.com>
2025-04-10 10:40:14 -0700

    Edited contact button to match homepage button

--- agile-blanche/src/app/components/Navbar.tsx
+++ agile-blanche/src/app/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
       className={classNames(
         item.name === 'Contact' ? 'font-[myFirstFont] rounded-full bg-[#171717] text-white text-lg hover:font-[myFirstFont]' : 'font-[myFirstFont]',
         item.current ? 'text-[#e9905a] bold-action-text drop-shadow-lg' : 'text-[#171717] transition delay-50 duration-200 ease-in-out hover:text-[#e9905a] hover:drop-shadow-lg',
-        'px-3 py-3 text-lg lg:text-xl custom-md-lg'
+        'px-3 py-2.5 text-lg lg:text-xl custom-md-lg'
       )}
     >
       {item.name}
