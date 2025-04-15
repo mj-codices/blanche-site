0f36dbbd4d7823b7e89c6c63fc7a95e4c16c2084
mj-codices <mjwhite.dev@gmail.com>
2025-04-15 14:33:29 -0700

    Styled service page and created action button

--- agile-blanche/src/app/components/ServiceCard.tsx
+++ agile-blanche/src/app/components/ServiceCard.tsx
@@ -1,6 +1,6 @@
 const serviceInfo = [
-    {name: "Web Development", description: "We build fast, scalable, and beautiful web applications tailored to your business."},
     {name: "Mobile Development", description: "We craft high-performance mobile apps that look great and feel even better."},
+    {name: "Web Development", description: "We build fast, scalable, and beautiful web applications tailored to your business."},
     {name: "App Support", description: "We offer ongoing app support to keep your product running smoothly as it grows."}
 ]
 
@@ -8,9 +8,18 @@ export default function ServiceCard() {
     return (
         <>
         {serviceInfo.map((item, index) => 
-            <div key={index} className="relative block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
-                <h2 className="absolute top-0 left-1/2 -translate-x-1/2">{item.name}</h2>
-                <p className="text-center">{item.description}</p>
+            <div key={index} className={item.name === 'Web Development' ? `
+                flex flex-col relative block w-75 h-95 px-4 shadow-lg border border-gray-200 rounded-lg bg-gray-100` : `
+                relative block w-60 h-80 px-2 bg-white shadow-lg border border-gray-200 rounded-lg text-[#88807B]`}>
+                <h2 className="text-center text-base pt-20 font-[myFirstFontBold] text-[#171717]">{item.name}</h2>
+                <p className="text-center text-sm pt-5">{item.description}</p>
+                {index === 1 && (
+                 
+                    <button 
+                    className="flex mt-auto mb-5 mx-25 px-3 pb-2 pt-3 justify-center text-xs rounded-full bg-[#e9905a] text-white font-[myFirstFontBold]"
+                    >MORE</button>)
+                    
+                    }
             </div>
         )}
         </>
--- agile-blanche/src/app/services/page.tsx
+++ agile-blanche/src/app/services/page.tsx
@@ -2,7 +2,7 @@ import ServiceCard from "../components/ServiceCard";
 
 export default function ServicesPage() {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center space-x-8 pt-15">
             <ServiceCard/>
         </div>
     )
