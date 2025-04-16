ac9afe5267cd85798a2fde7cad10ded7d2fcc50b
mj-codices <mjwhite.dev@gmail.com>
2025-04-15 22:04:46 -0700

    Imported react-icons into ServiceCard and styled them

--- agile-blanche/src/app/components/ServiceCard.tsx
+++ agile-blanche/src/app/components/ServiceCard.tsx
@@ -1,22 +1,31 @@
+import { FaMobileAlt, FaLaptopCode, FaHeadset } from "react-icons/fa";
+
 const serviceInfo = [
-    {name: "Mobile Development", description: "We craft high-performance mobile apps that look great and feel even better."},
-    {name: "Web Development", description: "We build fast, scalable, and beautiful web applications tailored to your business."},
-    {name: "App Support", description: "We offer ongoing app support to keep your product running smoothly as it grows."}
+    {   name: "Mobile Development", 
+        icon: FaMobileAlt, 
+        description: "We craft high-performance mobile apps that look great and feel even better."},
+    {   name: "Web Development", 
+        icon: FaLaptopCode,
+        description: "We build fast, scalable, and beautiful web applications tailored to your business."},
+    {   name: "App Support",
+        icon: FaHeadset,
+        description: "We offer ongoing app support to keep your product running smoothly as it grows."}
 ]
 
 export default function ServiceCard() {
+    
     return (
         <>
-        {serviceInfo.map((item, index) => 
-            <div key={index} className={item.name === 'Web Development' ? `
+        {serviceInfo.map((item, index) => {
+                const Icon = item.icon;
+                return (
+                <div key={index} className={item.name === 'Web Development' ? `
                 flex flex-col relative block w-75 h-95 px-4 shadow-lg border border-gray-200 rounded-lg bg-gray-100 text-[#88807B]` : `
                 relative block w-60 h-80 px-2 bg-white shadow-lg border border-gray-200 rounded-lg text-[#88807B]`}>
                 <div className={item.name === 'Web Development' ? `
                 flex bg-[#171717] rounded-lg mx-22 pt-2 px-1 absolute -top-7 size-24` : `
                 flex bg-[#171717] rounded-lg mx-20 py-1 px-1 absolute size-16 -top-5`}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-10 pb-3">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
-                    </svg>
+                    <Icon className={item.name ==='Web Development' ? `size-18 px-1 pt-1 pb-0 mx-3 text-white` : `size-10 p-1 m-2 text-white`}/>    
                 </div>
                 <h2 className={item.name === 'Web Development' ? `
                 text-center text-lg pt-27 font-[myFirstFontBold] text-[#171717]` : `
@@ -29,7 +38,8 @@ export default function ServiceCard() {
                     >MORE</button>)
                     
                     }
-            </div>
+            </div>)
+            }
         )}
         </>
     )
