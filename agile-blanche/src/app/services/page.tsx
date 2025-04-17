3d94e60b92cf0dc66351dd2261c535033d558986
mj-codices <mjwhite.dev@gmail.com>
2025-04-17 12:45:07 -0700

    Removed shadow from current on nav and added margin between service cards

--- agile-blanche/src/app/services/page.tsx
+++ agile-blanche/src/app/services/page.tsx
@@ -2,7 +2,7 @@ import ServiceCard from "../components/ServiceCard";
 
 export default function ServicesPage() {
     return (
-        <div className="flex items-center justify-center space-x-8 pt-15">
+        <div className="flex items-center justify-center space-x-12 pt-15">
             <ServiceCard/>
         </div>
     )
