3931b3287be477528957760b860e825e2bce3b4a
mj-codices <mjwhite.dev@gmail.com>
2025-04-25 05:10:10 -0700

    Set footer to sticky and servcard to flex-grow

--- agile-blanche/src/app/layout.tsx
+++ agile-blanche/src/app/layout.tsx
@@ -11,16 +11,18 @@ export default function RootLayout({
 
 
   return (
-    <html lang="en">
+    <html lang="en" className="overflow-x-hidden">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
         <link href="https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@98.1,300..700&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body className="overflow-x-hidden">
         <Navbar/>
-        {children}
+      <div className="flex flex-col min-h-[calc(100dvh-170px)]">
+        <main className="flex-grow">{children}</main>
         <Footer/>
+      </div>
       </body>
     </html>
   );
