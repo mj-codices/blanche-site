91468c60b90949947fdaf8790a7ffde526be6d93
mj-codices <mjwhite.dev@gmail.com>
2025-04-30 17:40:06 -0700

    Added Padding to Home Page

--- agile-blanche/src/app/page.tsx
+++ agile-blanche/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
     <section className="flex items-center justify-center">
-      <div className="pt-2 sm:pt-8 md:pt-10 lg:pt-15 mobile-landscape-padding">
+      <div className="pt-2 sm:pt-8 md:pt-10 lg:pt-15 lg:pb-7 mobile-landscape-padding">
         <span className="ml-5 sm:ml-0 fredoka text-7xl sm:text-7xl md:text-7xl lg:text-8xl tracking-wide">AGILE-</span>
         <span className="ml-5 sm:ml-0 fredoka text-6xl sm:text-7xl md:text-7xl lg:text-8xl text-[#88807B] tracking-wide">BLANCHE</span>
       </div>
