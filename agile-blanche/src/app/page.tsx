6de56b26db2f125db7eea5e5e111c9f7111bd9bd
mj-codices <mjwhite.dev@gmail.com>
2025-05-18 23:14:43 -0700

    Added blob btn to home page

--- agile-blanche/src/app/page.tsx
+++ agile-blanche/src/app/page.tsx
@@ -1,24 +1,28 @@
+"use client"
 import { Button } from '@headlessui/react'
+import { BlobButton } from './components/BlobButton/BlobButton';
 
 export default function Home() {
   return (
     <div>
     <section className="flex items-center justify-center">
-      <div className="pt-2 sm:pt-8 md:pt-10 lg:pt-15 lg:pb-7 mobile-landscape-padding">
-        <span className="ml-5 sm:ml-0 fredoka text-7xl sm:text-7xl md:text-7xl lg:text-8xl tracking-wide">AGILE-</span>
-        <span className="ml-5 sm:ml-0 fredoka text-6xl sm:text-7xl md:text-7xl lg:text-8xl text-[#88807B] tracking-wide">BLANCHE</span>
+      <div className="pt-2 sm:pt-15 md:pt-15 lg:pt-15 lg:pb-7 mobile-landscape-padding">
+        <span className="ml-5 sm:ml-0 fredoka text-7xl sm:text-6xl md:text-7xl lg:text-8xl tracking-wide">AGILE-</span>
+        <span className="ml-5 sm:ml-0 fredoka text-6xl sm:text-6xl md:text-7xl lg:text-8xl text-[#88807B] tracking-wide">BLANCHE</span>
       </div>
     </section>
     <section className="flex items-center justify-center">
       <div className="pt-6">
-        <span className="pl-2 pr-3 text-3xl sm:text-4xl md:text-4xl lg:text-6xl tracking-wider font-[myFirstFontBold] text-[#e9905a] mobile-landscape-hidden">Software</span>
-        <span className="pr-3 text-3xl sm:text-4xl md:text-4xl lg:text-6xl tracking-wider font-[myFirstFontBold] text-[#e9905a] mobile-landscape-hidden">Development</span>
-        <span className="block sm:inline pl-2 sm:pl-2 text-3xl sm:text-4xl md:text-4xl lg:text-6xl tracking-widest mobile-landscape-hidden">Services</span>
+        <span className="pl-2 pr-3 text-3xl sm:text-4xl md:text-5xl lg:text-6xl tracking-wider font-[myFirstFontBold] text-[#e9905a] mobile-landscape-hidden">Software</span>
+        <span className="pr-3 text-3xl sm:text-4xl md:text-5xl lg:text-6xl tracking-wider font-[myFirstFontBold] text-[#e9905a] mobile-landscape-hidden">Development</span>
+        <span className="block sm:inline pl-2 sm:pl-2 text-3xl sm:text-4xl md:text-5xl lg:text-6xl tracking-widest mobile-landscape-hidden">Services</span>
         <div className="flex sm:pt-10 items-center justify-center">
           <p className="invisible sm:visible font-[myFirstFontBold] lg:text-[#171717] text-xs sm:text-sm md:text-base lg:text-lg mobile-landscape-hidden">Redefining app development with modern, nimble and results-driven solutions.</p>
         </div>
-        <div className="flex items-center justify-center">
-            <Button className="rounded-full bg-[#171717] font-[myFirstFont] text-white transition delay-50 duration-150 ease-in-out hover:drop-shadow-lg text-lg pl-3.5 pr-3.5 pb-3 pt-3.5 mt-10 cursor-pointer">Contact Us</Button>
+        <div className="flex items-center justify-center pt-6 sm:pt-5 md:pt-8 lg:pt-10">
+            <BlobButton isHome={true} text="Contact Us" onClick={() => console.log('Contact button clicked!')} />
+              {/* Left button as reference */}
+            {/* <Button className="rounded-full bg-[#171717] font-[myFirstFont] text-white transition delay-50 duration-150 ease-in-out hover:drop-shadow-lg text-lg pl-3.5 pr-3.5 pb-3 pt-3.5 mt-10 cursor-pointer">Contact Us</Button> */}
         </div>
       </div>
     </section>
