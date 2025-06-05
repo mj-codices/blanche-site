7acb3bda373a2ae0e38c0ee40f8c9950d2b8c48b
mj-codices <mjwhite.dev@gmail.com>
2025-06-05 14:50:22 -0700

    Removed pulseGrow class from global and blobbutton

--- agile-blanche/src/app/components/BlobButton/BlobButton.tsx
+++ agile-blanche/src/app/components/BlobButton/BlobButton.tsx
@@ -26,7 +26,7 @@ export const BlobButton = ({
   return (
     <div
       onClick={onClick}
-      className={`${isActiveNav && isNav ? "pulse-grow" : ""} buttons p-2 sm:p-3 md:inline lg:p-3`}
+      className={` buttons p-2 sm:p-3 md:inline lg:p-3`}
     >
       <button
         className={`${isWhite ? "blob-btn-text-white blob-btn px-3 py-2 font-[myFirstFont] text-sm sm:px-4 sm:py-3 sm:text-base md:px-3 md:py-2 md:text-sm lg:px-4 lg:py-3 lg:text-lg" : ""} ${isBlack ? "blob-btn blob-btn-black px-3 py-2 font-[myFirstFont] text-sm sm:px-4 sm:py-3 sm:text-base md:px-3 md:py-2 md:text-sm lg:px-4 lg:py-3 lg:text-lg" : ""} ${isHome ? "blob-btn-text-home blob-btn px-4 py-3 font-[myFirstFont] text-lg hover:font-[myFirstFontBold]" : ""} ${isNav ? "blob-btn blob-btn-text font-[myFirstFont] text-sm sm:px-3 sm:py-2 md:px-4 md:py-3 md:text-base lg:px-4 lg:py-3 lg:text-lg" : ""} ${isActiveNav ? "blob-btn-text-active text-oscillate" : ""} ${disableHover ? "disable-hover" : ""}`}
