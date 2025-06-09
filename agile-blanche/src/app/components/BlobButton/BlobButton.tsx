a9449cb8e8d5fec0cab8fcbb0dd374735f804c9e
mj-codices <mjwhite.dev@gmail.com>
2025-06-09 09:16:12 -0700

    Reorganized button props and classes

--- agile-blanche/src/app/components/BlobButton/BlobButton.tsx
+++ agile-blanche/src/app/components/BlobButton/BlobButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useUIStore } from "@/app/store/ui-store";
 import "./BlobButton.css";
 
 interface BlobButtonProps {
@@ -26,10 +26,15 @@ export const BlobButton = ({
   return (
     <div
       onClick={onClick}
-      className={` buttons p-2 sm:p-3 md:inline lg:p-3`}
+      className={`buttons p-2 sm:p-3 md:inline lg:p-3`}
     >
       <button
-        className={`${isWhite ? "blob-btn-text-white blob-btn px-3 py-2 font-[myFirstFont] text-sm sm:px-4 sm:py-3 sm:text-base md:px-3 md:py-2 md:text-sm lg:px-4 lg:py-3 lg:text-lg" : ""} ${isBlack ? "blob-btn blob-btn-black px-3 py-2 font-[myFirstFont] text-sm sm:px-4 sm:py-3 sm:text-base md:px-3 md:py-2 md:text-sm lg:px-4 lg:py-3 lg:text-lg" : ""} ${isHome ? "blob-btn-text-home blob-btn px-4 py-3 font-[myFirstFont] text-lg hover:font-[myFirstFontBold]" : ""} ${isNav ? "blob-btn blob-btn-text font-[myFirstFont] text-sm sm:px-3 sm:py-2 md:px-4 md:py-3 md:text-base lg:px-4 lg:py-3 lg:text-lg" : ""} ${isActiveNav ? "blob-btn-text-active text-oscillate" : ""} ${disableHover ? "disable-hover" : ""}`}
+        className={`${isWhite ? "blob-btn-text-white blob-btn px-3 py-2 font-[myFirstFont] text-sm sm:px-4 sm:py-3 sm:text-base md:px-3 md:py-2 md:text-sm lg:px-4 lg:py-3 lg:text-lg" : ""} 
+                    ${isBlack ? "blob-btn blob-btn-black px-3 py-2 font-[myFirstFont] text-sm sm:px-4 sm:py-3 sm:text-base md:px-3 md:py-2 md:text-sm lg:px-4 lg:py-3 lg:text-lg" : ""} 
+                    ${isHome ? "blob-btn-text-home blob-btn px-4 py-3 font-[myFirstFont] text-lg hover:font-[myFirstFontBold]" : ""} 
+                    ${isNav ? "blob-btn blob-btn-text font-[myFirstFont] text-sm sm:px-3 sm:py-2 md:px-4 md:py-3 md:text-base lg:px-4 lg:py-3 lg:text-lg" : ""} 
+                    ${isActiveNav ? "blob-btn-text-active" : ""} 
+                    ${disableHover ? "disable-hover" : ""}`}
       >
         {text}
         <span
