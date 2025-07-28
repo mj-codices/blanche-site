0d68c374ddd8553e740592d4a4a8fdcb43578483
mj-codices <mjwhite.dev@gmail.com>
2025-07-27 21:36:08 -0700

    Updated ESLint config and dev packages

--- agile-blanche/eslint.config.js
+++ agile-blanche/eslint.config.js
@@ -1,21 +1,34 @@
 // eslint.config.js
 import js from '@eslint/js';
+import * as tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
 import next from 'eslint-config-next';
-import * as tseslint from 'typescript-eslint/eslint.config';
 
 export default [
-  js.configs.recommended,
-  next,
-  tseslint.configs.recommended,
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
     rules: {
-      // TEMPORARY disables for build success
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
+    },
+  },
+  {
+    rules: {
       'react/no-unescaped-entities': 'off',
       'react-hooks/exhaustive-deps': 'off',
       '@next/next/no-img-element': 'off',
     },
   },
+  js.configs.recommended,
+  next,
 ];
