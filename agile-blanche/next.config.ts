351da17461637ea7c43ce4f7102c994f133c838a
mj-codices <mjwhite.dev@gmail.com>
2025-07-27 21:49:48 -0700

    Added eslint override to next config

--- agile-blanche/next.config.ts
+++ agile-blanche/next.config.ts
@@ -1,14 +1,17 @@
 import type { NextConfig } from "next";
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
   webpack(config: Configuration) {
     // Exclude SVGs from the default file loader
-    const fileLoaderRule = config.module?.rules?.find((rule) =>
-      typeof rule === 'object' && rule?.test instanceof RegExp && rule.test.test('.svg')
+    const fileLoaderRule = config.module?.rules?.find(
+      (rule) =>
+        typeof rule === "object" &&
+        rule?.test instanceof RegExp &&
+        rule.test.test(".svg"),
     );
 
-    if (fileLoaderRule && typeof fileLoaderRule === 'object') {
+    if (fileLoaderRule && typeof fileLoaderRule === "object") {
       (fileLoaderRule as any).exclude = /\.svg$/i;
     }
 
@@ -16,10 +19,13 @@ const nextConfig: NextConfig = {
     config.module?.rules?.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
 
     return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
